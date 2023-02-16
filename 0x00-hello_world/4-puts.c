129913f851f8442a82db4e5877eecf116ebf149f
Tabbiely <maglynda21@gmail.com>
2023-02-16 11:50:44 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
+
 /**
  * main - a program that prints a line with puts function
  * Returns: 0 (success)
 */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
