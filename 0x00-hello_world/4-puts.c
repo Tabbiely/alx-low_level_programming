4040ee4d47da4036a402bbb0cf015f74f5e64c6e
Tabbiely <maglynda21@gmail.com>
2023-02-16 05:29:48 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-
+#include<stdio.h>
+/*
+ * main entry
+ * Returns 0
+ */
+   int main (void)
+  {
+	  puts("\" programming is like building a multi lingual puzzle");
+	  return(0);
+  }	  
